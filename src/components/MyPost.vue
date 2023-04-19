8a9bffcc671b1d1c45741e7a6d92b4c419dc989b
Adriel <adriel.fsantos@outlook.com>
2023-04-19 17:57:12 -0300

    Adiciona componente MyPost #7

---
+++ src/components/MyPost.vue
@@ -0,0 +1,115 @@
+<template>
+  <div :class="[detailed ? 'detailed' : '', 'myPost']">
+    <div class="myPost__top">
+      <h3>Não consigo mudar a versão do meu Node no linux, como faço?</h3>
+    </div>
+    <div class="myPost__bottom">
+      <h4 class="myPost__author">Gustavo</h4>
+      <div class="myPost__actions">
+        <button type="button" title="Favoritar">
+          <img src="@/assets/img/icons/icon-heart.svg" alt="Ícone de coração" />
+        </button>
+        <button type="button" title="Gostei">
+          <img
+            src="@/assets/img/icons/icon-thumbs-up.svg"
+            alt="Ícone de mão com dedão para cima"
+          />
+        </button>
+        <button type="button" title="Não gostei">
+          <img
+            src="@/assets/img/icons/icon-thumbs-down.svg"
+            alt="Ícone de mão com dedão para baixo"
+          />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyPost',
+  props: {
+    detailed: {
+      default: false,
+      type: Boolean,
+      required: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.myPost {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: var(--color-bg-300);
+  border-radius: 4px;
+}
+
+.myPost.detailed {
+  flex-direction: column;
+  align-items: stretch;
+}
+
+.myPost__top {
+  padding: 22px 25px 24px;
+}
+
+.myPost.detailed .myPost__top {
+  padding: 19px 23px 30px;
+}
+
+.myPost__top h3 {
+  font-weight: 700;
+  font-size: var(--font-size-normal);
+}
+
+.myPost.detailed .myPost__top h3 {
+  font-size: var(--font-size-normal-lg);
+}
+
+.myPost__bottom {
+  display: flex;
+  justify-content: space-between;
+  background-color: var(--color-bg-500);
+  align-self: stretch;
+}
+
+.myPost.detailed .myPost__bottom {
+  display: flex;
+  align-items: center;
+}
+
+.myPost__author {
+  display: none;
+}
+
+.myPost.detailed .myPost__author {
+  display: inline-block;
+  padding: 20px 18px;
+  color: var(--color-primary);
+  font-size: var(--font-size-normal);
+  font-weight: 600;
+}
+
+.myPost__actions {
+  padding: 28px 20px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+.myPost__actions button img {
+  width: 24px;
+  height: 24px;
+  object-fit: contain;
+}
+
+.myPost__actions.myPost.detailed button img {
+  width: 20px;
+  height: 20px;
+  object-fit: contain;
+}
+</style>
