9e6fad62edb95a0f2e42be260eff85e8af23de6e
Adriel <adriel.fsantos@outlook.com>
2023-04-18 23:44:48 -0300

    Implementa componente MySplashScreen #5

---
+++ src/components/MySplashScreen.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <img
+      src="@/assets/img/logo.png"
+      title="Logo do Forum BSI"
+      alt="Letra f maiúscula estilizada com tons da cor rosa"
+    />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'MySplashScreen',
+};
+</script>
+
+<style scoped>
+section {
+  min-width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+img {
+  width: 263px;
+  height: 263px;
+  object-fit: contain;
+}
+</style>
