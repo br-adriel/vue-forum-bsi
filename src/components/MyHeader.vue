b6908579427e7e45f84122e82e90fcd9be58dfd4
Adriel <adriel.fsantos@outlook.com>
2023-04-19 21:31:44 -0300

    Adiciona componente MyHeader #14

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <div class="container">
+      <p>Perfil do usuário</p>
+      <MySearchForm />
+    </div>
+  </header>
+</template>
+
+<script>
+import MySearchForm from './MySearchForm.vue';
+
+export default {
+  name: 'MyHeader',
+  components: { MySearchForm },
+};
+</script>
+
+<style scoped>
+header {
+  background-color: var(--color-bg-100);
+  padding: 16px 32px;
+  border-bottom: 1px solid var(--color-bg-500);
+}
+
+header .container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 16px;
+}
+</style>
