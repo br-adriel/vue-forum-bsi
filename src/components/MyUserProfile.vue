c24eec3f70b187e59a4d3895cbfba9be9121840e
Adriel <adriel.fsantos@outlook.com>
2023-04-21 21:01:32 -0300

    Implementa exibição de usuário ativo #32

--- src/components/MyUserProfile.vue
+++ src/components/MyUserProfile.vue
@@ -1,23 +1,53 @@
 <template>
-  <div>
-    <img src="@/assets/img/profile.jpg" alt="Sua imagem de perfil" />
-    <h4>Nome de usuário</h4>
+  <div v-if="user" class="user-info">
+    <img :src="user.photo" alt="Sua imagem de perfil" />
+    <div class="user-name">
+      <h4>{{ user.displayName }}</h4>
+      <button @click="signOut" type="button">Sair</button>
+    </div>
   </div>
+  <button v-else @click="openSignIn" type="button">Entrar</button>
 </template>
 
 <script>
 export default {
   name: 'MyUserProfile',
+  computed: {
+    user() {
+      return this.$store.getters.getUser;
+    },
+  },
+  methods: {
+    openSignIn() {
+      this.$store.dispatch('logUserIn');
+    },
+    signOut() {
+      this.$store.dispatch('logUserOut');
+    },
+  },
 };
 </script>
 
 <style scoped>
-div {
+button {
+  font-size: var(--font-size-sm);
+  padding: 0;
+  color: var(--color-primary);
+}
+
+.user-info {
   display: flex;
   align-items: center;
   gap: 16px;
   font-weight: 600;
   font-size: var(--font-size-normal);
+}
+
+.user-name {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  align-items: flex-start;
 }
 
 img {
